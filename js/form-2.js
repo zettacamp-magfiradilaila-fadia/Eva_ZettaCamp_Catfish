e26b45e6880b1ef3576f08982fb3db7eec9bbde6
firafadia28 <firafadia28@gmail.com>
2022-09-29 12:11:11 +0700

    form + table

---
+++ js/form-2.js
@@ -0,0 +1,16 @@
+var list1 = [];
+var list2 = [];
+var list3 = [];
+var list4 = [];
+var list5 = [];
+var list6 = [];
+var list7 = [];
+var list8 = [];
+
+var n = 1;
+var x = 0;
+
+function AddRow() {
+  var AddRown = document.getElementById('num');
+  var NewRow = AddRown.insertRow(n);
+}

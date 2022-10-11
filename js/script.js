0298e3e2fe348fa150ce3917a5c764ba5242a3a7
Magfira_Zettacamp <magfira.dIlaila@zettacamp.pro>
2022-10-11 12:28:49 +0700

    mod

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function purchaseBook(book_title, percentage_discount, percentage_tax) {
+  let price = 500000;
+
+  const amount_of_discount = (percentage_discount *= price / 100);
+  console.log('Amount of Discount: ', amount_of_discount);
+
+  const price_after_discount = price - amount_of_discount;
+  console.log('Price after discount: ', price_after_discount);
+
+  const amount_of_tax = (percentage_tax * price) / 100;
+  console.log('Amount of tax: ', amount_of_tax);
+
+  const price_after_tax = price + amount_of_tax;
+  console.log('Price after tax: ', price_after_tax);
+}
+
+console.log(purchaseBook('Harry Potter', 20, 30));

a66b255340e5f2fcb16731f10b2f64ce8140d9fe
Magfira_Zettacamp <magfira.dIlaila@zettacamp.pro>
2022-10-19 12:49:35 +0700

    mod

--- js/script.js
+++ js/script.js
@@ -39,9 +39,52 @@ app.get('/bookPurchase', function (req, res) {
   console.log('Async is done');
 });*/
 
-//app.get('/no-await', (req, res) => {});
-//app.get('/await', (req, res) => {});
-//app.get('/sets-maps', (req, res) => {});
+app.get('/no-await', (req, res) => {
+  const fs = require('fs');
+  const events = require('events');
+  const eventEmitter = new events.EventEmitter();
+  //console.log(eventEmitter);
+  function readDataFile(filename) {
+    const result = fs.readFile(filename, 'utf8');
+    console.log(result);
+  }
+  readDataFile('./lorem.txt');
+
+  const readFileDataAwait = async (fileName) => {
+    const result = await readDataFile(fileName);
+    console.log(result);
+  };
+  readDataFile('./lorem.txt');
+
+  eventEmitter.on('afterText', readDataFile);
+  eventEmitter.on('read await', readFileDataAwait);
+  eventEmitter.on('read await', './lorem.txt');
+});
+
+app.get('/await', (req, res) => {
+  const fs = require('fs').promises;
+  function readDataFile(filename) {
+    const result = fs.readFile(filename, 'utf8');
+    console.log(result);
+  }
+  readDataFile('./lorem.txt');
+
+  const readFileData = (dataPromise) => {
+    const result = dataPromise.then((data) => console.log(data));
+  };
+
+  readFileData(readDataFile('./lorem.txt'));
+
+  const readFileDataAwait = async (fileName) => {
+    const result = await readDataFile(fileName);
+    console.log(result);
+  };
+
+  readFileDataAwait('./lorem.txt');
+});
+
+app.get('/sets-maps', (req, res) => {});
+
 app.listen(port);
 function purchaseBook(book_title, percentage_discount, percentage_tax, credit_term, additional_term) {
   let price = 500000;

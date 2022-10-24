582cc9a92730a219b6aafba0f71648ea2cb559ca
Magfira_Zettacamp <magfira.dIlaila@zettacamp.pro>
2022-10-24 09:44:13 +0700

    revising

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const { request } = require('http');
 const app = express();
 const port = 8000;
 
 app.get('/bookPurchase', function (req, res) {
   const auth = req.headers['authorization'];
-  let { book_title, percentage_discount, percentage_tax, credit_term } = req.query;
-  console.log(book_title, percentage_discount, percentage_tax, credit_term);
+  let { book_title, percentage_discount, percentage_tax, credit_term, additional_term } = req.query;
+  //console.log(book_title, percentage_discount, percentage_tax, credit_term, additional_term);
 
   const userpass = auth.split(' ')[1];
   //console.log(userpass);
@@ -19,7 +20,7 @@ app.get('/bookPurchase', function (req, res) {
   //console.log(password);
 
   if (username == 'firafadia28' && password == 'justfira28') {
-    let result = purchaseBook(book_title, percentage_discount, percentage_tax, credit_term);
+    let result = purchaseBook(book_title, percentage_discount, percentage_tax, credit_term, additional_term);
 
     res.send(result);
   } else {
@@ -27,35 +28,14 @@ app.get('/bookPurchase', function (req, res) {
   }
 });
 
-/*app.get('/async-bookPurchase', async (req, res) => {
-  const timeout = setTimeout(() => {
-    console.log('Done');
-  }, 3000);
-  const result = await calculatePriceTerm();
-  console.log(result);
+app.get('/async-bookPurchase', async (req, res) => {
+  let result_2 = bookPurchase(6, 2000);
+  const result = await calculatePriceTerm(6, 2000);
+  console.log(result_2);
   console.log('Async is done');
-});*/
-
-app.get('/set', (req, res) => {
-  const bookSet = new Set(['Harry Potter', 'Percy Jackson', 'Da Vinci Code', 'The Kane Chronicles', 'Lord of The Rings']);
-  //console.log(bookSet);
-  res.send(...bookSet);
-});
-
-app.get('/map', (req, res) => {
-  const bookMap = new Map([
-    ['book_1', 'Eragon'],
-    ['book_2', 'Frozen'],
-    ['book_3', 'Tintin'],
-    ['book_4', 'Apollo'],
-    ['book_5', 'Osas'],
-  ]);
-  //console.log(animalMap);
-  let text;
-  bookMap.forEach(function (value, key) {
-    text += key + ': ' + value;
+  result_2.then((param) => {
+    console.log(param);
   });
-  res.send(...bookMap);
 });
 
 app.listen(port);
@@ -74,26 +54,44 @@ function purchaseBook(book_title, percentage_discount, percentage_tax, credit_te
   const price_after_tax = price + amount_of_tax;
   console.log('Price after tax: ', price_after_tax);
 
-  /*let arrayResult = [];
-  var total = price / credit_term;
-  for (let i = 1; i <= credit_term; i++) {
-    arrayResult.push({
-      price: total,
-      month: i,
-    });
-  }
-  return arrayResult;*/
-}
-
-/*async function bookPurchase(credit_term, additional_term) {
   let arrayResult = [];
   var total = price / credit_term;
-  var totalPriceTerm = total + additional_term;
-  for (let i = 1; i <= credit_term; i++) {
+  var totalPerTerm = total + additional_term;
+  for (i = 0; i <= credit_term; i++) {
+    if (i === 3) {
+      let resultTerm = calculatePriceTerm(total, additional_term);
+      console.log(resultTerm);
+      console.log(total);
+      console.log(additional_term);
+    }
     arrayResult.push({
       price: total,
       month: i,
     });
   }
+
+  /*for (let i = 1; i <= credit_term; i++) {
+    arrayResult.push({
+      price: total,
+      month: i,
+    });
+  }*/
   return arrayResult;
-}*/
+}
+
+async function bookPurchase(credit_term, additional_term) {
+  var totalPerTerm = credit_term + additional_term;
+  var result;
+  for (i = 0; i <= credit_term; i++) {
+    if (i === 3) {
+      result = await calculatePriceTerm(totalPerTerm, 2000);
+    }
+  }
+  return result;
+}
+
+function calculatePriceTerm(priceCredit, additionalTerm) {
+  let term = additionalTerm + priceCredit;
+
+  return term;
+}

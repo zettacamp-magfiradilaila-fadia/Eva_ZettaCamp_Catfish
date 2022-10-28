3bcfba39b17921a62d61ae5cc0456ef40360f374
Magfira_Zettacamp <magfira.dIlaila@zettacamp.pro>
2022-10-28 07:52:26 +0700

    doing task day 3

--- js/app.js
+++ js/app.js
@@ -21,5 +21,93 @@ app.use(
     extended: true,
   })
 );*/
+//------------------------------ DAY 2 ------------------------------
+app.get('/save-book', (req, res) => {
+  /*const book_1 = new BookModel({ title: 'Percy Jackson and The Olympians : The Lightning Thief', author: 'Rick Riordan', date_published: '28/06/2005', price: 85000 });
+  const result = book_1.save((err, book) => {
+    if (err) {
+      console.log('failed to insert');
+    } else {
+      console.log(book.title + ' saved successfully');
+    }
+  });*/
+  var bookList = [
+    { title: 'Percy Jackson and The Olympians : The Titans Curse', author: 'Rick Riordan', date_published: '11/05/2007', price: 85000 },
+    { title: 'Percy Jackson and The Olympians : The Battle of Labyrinth', author: 'Rick Riordan', date_published: '06/05/2008', price: 85000 },
+    { title: 'Percy Jackson and The Olympians : The Last Olympian', author: 'Rick Riordan', date_published: '05/05/2009', price: 85000 },
+    { title: 'The Kane Chronicles : The Red Pyramid', author: 'Rick Riordan', date_published: '04/05/2010', price: 90000 },
+    { title: 'The Kane Chronicles : The Throne of Fire', author: 'Rick Riordan', date_published: '04/05/2011', price: 90000 },
+    { title: 'The Kane Chronicles : The Serpent Shadow', author: 'Rick Riordan', date_published: '01/05/2012', price: 90000 },
+    { title: 'The Heroes of Olympus : The Lost Hero', author: 'Rick Riordan', date_published: '12/10/2010', price: 95000 },
+    { title: 'The Heroes of Olympus : The Son of Neptune', author: 'Rick Riordan', date_published: '04/10/2011', price: 95000 },
+    { title: 'The Heroes of Olympus : The Mark of Athena', author: 'Rick Riordan', date_published: '02/10/2012', price: 95000 },
+    { title: 'The Heroes of Olympus : The House of Hades', author: 'Rick Riordan', date_published: '08/10/2013', price: 95000 },
+    { title: 'The Heroes of Olympus : The Blood of Olympus', author: 'Rick Riordan', date_published: '07/10/2014', price: 95000 },
+    { title: 'The Trials of Apollo : The Hidden Oracle', author: 'Rick Riordan', date_published: '03/05/2016', price: 90000 },
+    { title: 'The Trials of Apollo : The Dark Prophecy', author: 'Rick Riordan', date_published: '02/05/2017', price: 90000 },
+    { title: 'The Trials of Apollo : The Burning Maze', author: 'Rick Riordan', date_published: '01/05/2018', price: 90000 },
+    { title: 'The Trials of Apollo : The Tyrants Tomb', author: 'Rick Riordan', date_published: '24/09/2019', price: 90000 },
+    { title: 'The Trials of Apollo : The Tower of Nero', author: 'Rick Riordan', date_published: '06/10/2020', price: 90000 },
+    { title: 'Magnus Chase and the Gods of Asgard : The Sword of Summer', author: 'Rick Riordan', date_published: '06/10/2015', price: 100000 },
+    { title: 'Magnus Chase and the Gods of Asgard : The Hammer of Thor', author: 'Rick Riordan', date_published: '04/10/2016', price: 100000 },
+    { title: 'Magnus Chase and the Gods of Asgard : The Ship of The Dead', author: 'Rick Riordan', date_published: '03/10/2017', price: 100000 },
+  ];
+  const result = BookModel.insertMany(bookList, (err, data) => {
+    if (err) {
+      console.log('failed to insert');
+    } else {
+      console.log('successfully inserted');
+    }
+  });
+  res.send(result);
+});
 
+app.post('/insert-book', (req, res) => {
+  const { title, author, date_published, price } = req.query;
+  const newBook = new BookModel({
+    title: title,
+    author: author,
+    date_published: date_published,
+    price: price,
+  });
+  const result = newBook.save();
+  res.send(result);
+});
+
+app.get('/find-all', (req, res) => {
+  const result = BookModel.find();
+  res.send(result);
+});
+//-------------------------------------------------------------------
+
+//------------------------------ DAY 3 ------------------------------
+app.get('/get-all-bookshelf', (req, res) => {
+  const result = BookshelfModel.find({
+    book_id: mongoose.Types.ObjectId(),
+  });
+  res.send(result);
+});
+
+app.post('/insert-bookshelf', (req, res) => {
+  const { name, book_id } = req.query;
+  const newBookshelf = BookshelfModel({
+    name: name,
+    book_ids: mongoose.Types.ObjectId(),
+  });
+  const result = newBookshelf.save();
+  res.send(result);
+});
+
+app.post('/insert-bookshelf-2', (req, res) => {
+  const bookshelfList = new BookshelfModel({
+    name: 'Bookshelf 1',
+    book_ids: [mongoose.Types.ObjectId('6358ef5196fa324369dba9a6'), mongoose.Types.ObjectId('6358f3cabd7e94a21b0f6618')],
+  });
+  const result = BookshelfModel.insertMany(bookshelfList);
+  console.log(result);
+  res.send(result);
+});
+//------------------------------ DAY 4 ------------------------------
+//------------------------------ DAY 5 ------------------------------
+//------------------------------ DAY 6 ------------------------------
 app.listen(port);

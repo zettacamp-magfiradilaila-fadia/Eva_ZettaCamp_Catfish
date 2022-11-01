86d0019b9efa8e82620e187831517d7005630d0b
Magfira_Zettacamp <magfira.dIlaila@zettacamp.pro>
2022-11-01 16:55:08 +0700

    new model

--- js/shelfbook_model.js
+++ js/shelfbook_model.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 const shelfbook = new mongoose.Schema({
-    name: {type: String},
-    book_ids: [{
-        book_id: {type: mongoose.Schema.Types.ObjectId},
-        added_date: {},
-    }]
-    stock
-})
+  name: { type: String },
+  book_ids: [
+    {
+      book_id: { type: mongoose.Schema.Types.ObjectId },
+      added_date: {},
+    },
+  ],
+});

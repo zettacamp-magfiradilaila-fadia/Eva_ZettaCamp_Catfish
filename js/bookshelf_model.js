3cfd4e8e4e7636313b09b46fd31cb9ab750d8916
Magfira_Zettacamp <magfira.dIlaila@zettacamp.pro>
2022-11-01 16:54:53 +0700

    new model

--- js/bookshelf_model.js
+++ js/bookshelf_model.js
@@ -5,7 +5,7 @@ const bookshelfSchema = new Schema(
     name: { type: String },
     book_list: [
       {
-        book_ids: { type: Schema.ObjectID, ref: 'books' },
+        book_ids: { type: Schema.ObjectId, ref: 'books' },
       },
     ],
   },
